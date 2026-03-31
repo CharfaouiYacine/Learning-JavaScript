90f5c1433bff907c4c63f7d7fd5f265431538f00
Charfaoui Yacine <yacineecharfaoui@gmail.com>
2026-03-31 10:18:02 +0100

    Ternary operator

---
+++ learning.js
@@ -0,0 +1,4 @@
+var isGoing = false
+var color = isGoing ? "green" : "red";
+console.log(color);
+//Ternary operator : var + varName = variable To check  + ? + value to take if true + : + value to take if false
