91d9dffbfb05efc8e7d8457ba471700f9193dda2
Charfaoui Yacine <yacineecharfaoui@gmail.com>
2026-03-31 20:52:33 +0100

    loops added

--- learning.js
+++ learning.js
@@ -22,3 +22,34 @@ switch (option) { // if you don't use break statement you'll run to a problem ca
             // one  before it or else it's gonna have a lot of  cases printed
         console.log("so you die");
 }
+
+// Loops
+// While loop structure:
+var i = 1
+while(/*condition*/ i < 10){
+    // here goes your  instructions
+    console.log(i)
+    // don't forget to update the condition or use break statement or else you'll end up with an infinite loop
+    i = i + 1
+}
+// For loop
+for(var j = 0; j < 3; j++){
+    // your instructions go here
+    console.log(j)
+}
+// Nested loops
+for(var k = 0; k < 3; k++){
+    for(var l = 0; l < 3; l++){
+        console.log(k+","+l)
+        // it will start printing: 0,0 ; 0,1 ........until 2,2
+    }
+}
+// increment or decrement shortcuts:
+/*
+x++ or ++x // same as x = x + 1
+x-- or --x // same as x = x - 1
+x += 3 // same as x = x + 3
+x -= 6 // same as x = x - 6
+x *= 2 // same as x = x*  2
+x /= 5 // same as x = x / 5
+*/
