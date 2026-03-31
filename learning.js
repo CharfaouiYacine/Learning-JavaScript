dc028c5e05f52abb2859fa13dd088075717a251c
Charfaoui Yacine <yacineecharfaoui@gmail.com>
2026-03-31 18:31:19 +0100

    added the switch statement method

--- learning.js
+++ learning.js
@@ -2,3 +2,23 @@ var isGoing = false
 var color = isGoing ? "green" : "red";
 console.log(color);
 //Ternary operator : var + varName = variable To check  + ? + value to take if true + : + value to take if false
+
+
+// switch statement
+var option = 3
+
+switch (option) { // if you don't use break statement you'll run to a problem called : falling through which means you'll
+    // print the case and all the cases after it
+    case 1:
+        console.log("you chose 1");
+        break;
+    case 2:
+        console.log("you chose 2");
+        break;
+    case 3:
+        console.log("you chose 3");
+        break;
+    default: // can you this to add a default message that prints after a case  , the problem is that the case should be the
+            // one  before it or else it's gonna have a lot of  cases printed
+        console.log("so you die");
+}
