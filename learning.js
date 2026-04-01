7c9204cf41d5a4a74dd04fb5d464c8031bad699f
Charfaoui Yacine <yacineecharfaoui@gmail.com>
2026-04-01 13:24:34 +0100

    functions section added

--- learning.js
+++ learning.js
@@ -53,3 +53,46 @@ x -= 6 // same as x = x - 6
 x *= 2 // same as x = x*  2
 x /= 5 // same as x = x / 5
 */
+
+// Functions:
+// syntax:
+function add(a, b){
+    return a + b;
+}
+/*
+**If an identifier is declared in global scope, it's available everywhere.
+
+**If an identifier is declared in function scope, it's available in the
+function it was declared in (even in functions declared inside the function).
+
+**When trying to access an identifier, the JavaScript Engine will first look in the current function. If it doesn't
+find anything, it will continue to the next outer function to see if it can find the identifier there,
+It will keep doing this until it reaches the global scope.
+
+**Global identifiers are a bad idea. They can lead to bad variable names, conflicting variable names, and messy code.
+
+**shadowing mean: when a variable declared within a specific inner scope (like a function or a block)
+has the same name as a variable in an outer scope.
+
+
+** hoisting mean: a behavior where the interpreter appears to move the declarations of functions, variables,
+and classes to the top of their containing scope during the compilation phase, before the code is executed.
+
+**Variable assignments are not hoisted
+*/
+
+// function expressions:
+//Function Expression: When a function is assigned to a variable. The function can be named,
+//or anonymous. Use the variable name to call a function defined in a function expression.
+/*
+var catSays = function(max) {
+  var catMessage = "";
+  for (var i = 0; i < max; i++) {
+    catMessage += "meow ";
+  }
+  return catMessage;
+}
+*/
+
+
+
