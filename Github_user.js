4feb3ff05ae1cd7963d681d03ea209d970db29fb
Charfaoui Yacine <yacineecharfaoui@gmail.com>
2026-04-18 21:12:48 +0100

    Added  handling for the response fetching

--- Github_user.js
+++ Github_user.js
@@ -10,58 +10,63 @@ let api_url = "https://api.github.com/users/"
 async function get_infos(user){
     try{
         let response = await  fetch(`${api_url}${user}`)
-        const data = await response.json()
-        console.log(`       UserName: ${data['login']}
-        URL: ${data['url']}
-        Location: ${data['location']}
-        Bio: ${data['bio']}
-        Followers: ${data['followers']}
-        Following: ${data['following']}
-        Public Repos: ${data['public_repos']}`)
-
+        if(response.ok) {
+            const data = await response.json()
+                console.log(`UserName: ${data['login']}\nURL: ${data['url']}\nLocation: ${data['location']}\nBio: ${data['bio']}\nFollowers: ${data['followers']}\nFollowing: ${data['following']}\nPublic Repos: ${data['public_repos']}`)
+        }
+        else{
+            console.log(`Error ${response.status}`)
+        }
     }catch(error){
-        let response = await fetch(`${api_url}${user}`)
-        console.log(`Something went wrong: ${error.name}/${response.status}`)
+        console.log(`Something went wrong :(`)
     }
 }
 async function get_repos(user){
     try{
         let response = await fetch(`${api_url}${user}/repos`)
-        const repos = await response.json()
-        if(repos.length ===0){
-            console.log("-----No repos Yet---------")
-        }else {
-            console.log("Some Repos: ")
-            for (var i = 0; i < 10; i = i + 1) {
-                if(repos[i]===null || repos[i]===undefined){
+        if(response.ok) {
+            const repos = await response.json()
+            if (repos.length === 0) {
+                console.log("-----No repos Yet---------")
+            } else {
+                console.log("Some Repos: ")
+                for (let i = 0; i < 10; i = i + 1) {
+                    if (repos[i] === null || repos[i] === undefined) {
 
-                }
-                else{
-                    console.log(`${i + 1}) ${repos[i]['html_url']}`)
+                    } else {
+                        console.log(`${i + 1}) ${repos[i]['html_url']}`)
+                    }
                 }
             }
         }
+        else{
+            console.log(`Error ${response.status}`)
+        }
     }catch (err){
-        let response = await fetch(`${api_url}${user}/repos`)
-        console.log(`Something went wrong: ${err.name}/${response.status}`)
+        console.log(`Something went wrong :(`)
     }
 }
 async function get_activity(user){
     try{
         let response = await fetch(`${api_url}${user}/events`)
-        const activity = await response.json()
-        if(activity.length === 0){
-            console.log("-----No Activity Yet--------")
-        }
-        else{
-            console.log("Recent Events: ")
-            for (var i = 0; i < 5; i = i + 1) {
-                console.log(`- ${activity[i]['type']} in ${activity[i]['repo']['name']}, URl: ${activity[i]['repo']['url']}`)
+        if(response.ok) {
+            const activity = await response.json()
+            if (activity.length === 0) {
+                console.log("-----No Activity Yet--------")
+            } else {
+                console.log("Recent Events: ")
+                for (let i = 0; i < 5; i = i + 1) {
+                    if (activity[i] === null || activity[i] === undefined) {
+                    } else {
+                        console.log(`- ${activity[i]['type']} in ${activity[i]['repo']['name']}, URl: ${activity[i]['repo']['url']}`)
+                    }
+                }
             }
+        }else{
+            console.log(`Error ${response.status}`)
         }
     }catch(err){
-        let response = await fetch(`${api_url}${user}/events`)
-        console.log(`Something went wrong: ${err.name}/${response.status}`)
+        console.log(`Something went wrong :(`)
     }
 }
 function separator(){
@@ -70,7 +75,7 @@ function separator(){
 
 async function main(){
     let name = input("Enter the name of the user you want to check : ")
-    while(name.length<1 || typeof name == 'number'){
+    while(name.length<1){
         name = input("Please enter a valid name : ")
     }
     await get_infos(name)
