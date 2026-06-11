322e18047ec25e5288fdbd8f3e736ff4b9721544
Charfaoui Yacine <yacineecharfaoui@gmail.com>
2026-06-11 19:03:02 +0100

    changed all the functions to work with Postgresql database instead of using a file

--- Todo_List.js
+++ Todo_List.js
@@ -1,131 +1,128 @@
+const {Client} = require('pg')
+const client = new Client({
+    host: "localhost",
+    user: "postgres",
+    port: "5432",
+    password: "My password",
+    database: "todo"
+})
+client.connect()
+
 const ps = require("prompt-sync")
 const input = ps()
 
-const fs = require("fs")
-
-
-let tasks = []
-
-function separator(){
+function separator() {
     console.log("==========================================")
 }
-function add_task(){
+
+async function add_task() {
     let task = input("Enter the task you want to add: ")
-    tasks.push(task)
-    console.log("Task Added!!!!")
-    update_file()
+    try {
+        await client.query('INSERT INTO todolist (name) VALUES ($1)', [task])
+        console.log('Task Added')
+    } catch (err) {
+        console.log(`Database Error: ${err.message}`)
+    }
     separator()
-
-}
-function delete_task(){
-    if(tasks.length ===0){
-        console.log("The list is empty")
-        separator()
-    }
-    else{
-        let task_num = Number(input("Enter the number of the task to delete: "))
-        while(task_num<1 || task_num>tasks.length){
-            task_num = Number(input("Please enter a valid number: "))
-        }
-        tasks.splice(task_num-1,1)
-        console.log("Task Deleted!!!!")
-        update_file()
-        separator()
-    }
-}
-function modify_task(){
-    if(tasks.length ===0){
-        console.log("The list is empty")
-        separator()
-    }
-    else {
-        let task_num = Number(input("Enter the number of the task to Modify: "))
-        while(task_num<1 || task_num>tasks.length){
-            task_num = Number(input("Please enter a valid number: "))
-        }
-        tasks[task_num - 1] = input("Enter the new task: ")
-        console.log("Task is modified!!!!!!")
-        update_file()
-        separator()
-    }
-}
-function show_tasks(){
-    if(tasks.length ===0){
-        console.log("The list is empty")
-        separator()
-    }
-    else{
-        for(let i=0;i<tasks.length;i++){
-            console.log(`${i+1}) ${tasks[i]}`)
-        }
-        separator()
-    }
-}
-function mark_as_done(){
-    if(tasks.length ===0){
-        console.log("The list is empty")
-        separator()
-    }
-    else{
-        let task_num = Number(input("Enter the number of the task to mark: "))
-        while(task_num<1 || task_num>tasks.length){
-            task_num = Number(input("Please enter a valid number: "))
-        }
-        if(tasks[task_num-1].includes('[x]')){
-            console.log("Task is already Done")
-            separator()
-        }
-        else{
-        tasks[task_num-1] = tasks[task_num-1] +"[x]"
-        console.log("Task is marked as done!!!!!!")
-        update_file()
-        separator()
-        }
-    }
 }
 
-function update_file(){
-    fs.writeFileSync("tasks.txt","")
-    for(var i=0 ; i<tasks.length ; i++){
-    fs.appendFileSync("tasks.txt",`${tasks[i]}\n`)
-    }
-}
-function  load_tasks(){
-    const data = fs.readFileSync("tasks.txt","utf-8")
-    let task_line = data.split("\n")
-    for(var i=0;i<task_line.length;i++){
-        if(task_line[i].length ===0){
-
-        }else {
-            tasks.push(task_line[i])
+async function delete_task() {
+    try{
+        let id_num = Number(input('Enter the id of the task you want to delete: '))
+        let result = await client.query(`DELETE FROM todolist  WHERE id = $1`,[id_num])
+        if (result.rowCount ===0){
+            console.log("The task doesn't exist")
+        }else{
+            console.log('Task deleted')
         }
+
+    }catch (err){
+        console.log(`DataBase Error:${err.message}`)
     }
+    separator()
 }
 
-function main() {
+async function modify_task() {
+    try {
+        let id_num = Number(input('Enter the id of the task you want to modify: '))
+        let new_task = input('Enter the new task: ')
+        while (new_task.length === 0) {
+            new_task = input('please enter a non-empty task: ')
+        }
+        let result = await client.query(`UPDATE todolist SET name = $1 WHERE id = $2`, [new_task, id_num])
+        if (result.rowCount === 0) {
+            console.log("The task doesn't Exist")
+        } else {
+            console.log('Task Modified')
+        }
+
+    } catch (err) {
+        console.log(`Error: ${err.message}`)
+    }
+    separator()
+}
+
+async function show_tasks() {
+    try {
+        let data = await client.query('SELECT * FROM todolist')
+        let data_rows = data.rows
+        if (data_rows.length === 0) {
+            console.log('The List is empty')
+        } else {
+            for (let i = 0; i < data_rows.length; i++) {
+                console.log(`[${i + 1}]==>  Task_id: ${data_rows[i].id}\nTask: ${data_rows[i].name} \nStatus : ${data_rows[i].completed}`)
+                console.log('-----------------------------------')
+            }
+        }
+    } catch (err) {
+        console.log(`Database Error: ${err.message}`)
+    }
+    separator()
+}
+
+async function mark_as_done() {
+    try {
+        let id_num = Number(input('Enter the id of the task you want to mark as done: '))
+        let result = await client.query(`UPDATE todolist SET completed = TRUE WHERE id = $1`, [id_num])
+        if (result.rowCount === 0) {
+            console.log("Task or Id doesn't exist")
+        } else {
+            console.log('Task marked as Done')
+        }
+    } catch (err) {
+        console.log(`An error Occurred: ${err.message}`)
+    }
+    separator()
+}
+
+
+async function main() {
     let choice;
     let menu = "1-Add a task\n2-Delete a task\n3-Modify a task\n4-Show The tasks\n5-mark as done\n6-Exit"
-    load_tasks()
     while (true) {
         console.log(menu)
         choice = Number(input("Enter the number of task you want to do: "))
+        separator()
         while (choice > 6 || choice < 1) {
             choice = Number(input("Please enter a valid number: "))
+            separator()
         }
 
         if (choice === 1) {
-            add_task()
+            await add_task()
         } else if (choice === 2) {
-            delete_task()
+            await delete_task()
         } else if (choice === 3) {
-            modify_task()
+            await modify_task()
         } else if (choice === 4) {
-            show_tasks()
+            await show_tasks()
         } else if (choice === 5) {
-            mark_as_done()
-        }else{
+            await mark_as_done()
+        } else {
+            await client.end()
             break
         }
     }
 }
+
 main()
